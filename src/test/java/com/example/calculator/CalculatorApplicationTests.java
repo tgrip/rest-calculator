295828de0cb2c1dea8646296f738609024883b37
theodor.grip <theodor@chrono24.com>
2019-11-04 19:32:01 +0100

    Initial commit

---
+++ src/test/java/com/example/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
